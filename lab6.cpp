a1f425ea4204072b112e43a1d95008cf5641aa3f
Enkh-Orgil1057 <u.enhorgil@gmail.com>
2026-04-06 13:07:44 +0800

    orgil

--- lab6.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-// ehleed uurchlultuu hiicheed "git add ."" oruulaad
-// tgd "git pull" hiigeed
-// draa ni "git commit" hiigeed
-// garch irsn filed ni ngo door bgag huulj saveleed
-// "commit https://github.com/Enkh-Orgil1057/OOP-Whiplash"
-// tgd "git push" gj bichih yum shig bnshd nzuda
